c5f8d1e0e7339d63f702275efff3890ec4571718
Michael Milton <ttmigueltt@gmail.com>
2022-10-27 12:58:00 +1100

    Fix issue 20

--- dev/metadata_database.R
+++ dev/metadata_database.R
@@ -2,7 +2,7 @@ args <- commandArgs(trailingOnly = TRUE)
 # Path to the metadata file used as input
 metadata_input <- args[[1]]
 # Path to the sqlite file which this script will output
-output_file <- args[[1]]
+output_file <- args[[2]]
 
 metadata <- readRDS(metadata_input)
 
