af560a4447365f452ee9b08ecc91ffb8ecef7823
Michael Milton <ttmigueltt@gmail.com>
2023-03-09 16:11:04 +1100

    R CMD check fixes

--- R/unharmonised.R
+++ R/unharmonised.R
@@ -4,9 +4,13 @@
 #' make sense for these to live in the main metadata table. This function is a
 #' utility that allows easy fetching of this data if necessary.
 #'
-#' @param dataset_ids A character vector, where each entry is a dataset ID
+#' @param dataset_id A character vector, where each entry is a dataset ID
 #'   obtained from the `$file_id` column of the table returned from
 #'   [get_metadata()]
+#' @param cells An optional character vector of cell IDs. If provided, only
+#'   metadata for those cells will be returned.
+#' @param conn An optional DuckDB connection object. If provided, it will re-use
+#'   the existing connection instead of opening a new one.
 #' @param remote_url Optional character vector of length 1. An HTTP URL pointing
 #'   to the root URL under which all the unharmonised dataset files are located.
 #' @param cache_directory Optional character vector of length 1. A file path on
@@ -17,12 +21,13 @@
 #' @importFrom DBI dbConnect
 #' @importFrom duckdb duckdb
 #' @importFrom dplyr tbl filter
+#' @importFrom rlang .data
 #' @return A named list, where each name is a dataset file ID, and each value is
 #'   a "lazy data frame", ie a `tbl`.
 #' @examples
 #' dataset = "838ea006-2369-4e2c-b426-b2a744a2b02b"
 #' harmonised_meta = get_metadata() |> dplyr::filter(file_id == dataset) |> dplyr::collect()
-#' unharmonised_meta = get_unharmonised_metadata_list(dataset)
+#' unharmonised_meta = get_unharmonised_dataset(dataset)
 #' unharmonised_tbl = dplyr::collect(unharmonised_meta[[dataset]])
 #' dplyr::left_join(harmonised_meta, unharmonised_tbl, by=c("file_id", "cell_"))
 get_unharmonised_dataset = function(
@@ -41,7 +46,7 @@ get_unharmonised_dataset = function(
             progress(type = "down", con = stderr())
         )
     tbl(conn, local_path) |>
-        filter(cell_ %in% cells)
+        filter(.data$cell_ %in% cells)
 }
 
 #' Returns unharmonised metadata for a metadata query
@@ -54,6 +59,7 @@ get_unharmonised_dataset = function(
 #'  * `unharmonised`: a nested tibble, with one row per cell in the input `metadata`, containing unharmonised metadata
 #' @export
 #' @importFrom dplyr group_by summarise filter collect
+#' @importFrom rlang .data
 #' @examples
 #' harmonised <- get_metadata() |> dplyr::filter(tissue == "kidney blood vessel")
 #' unharmonised <- get_unharmonised_metadata(harmonised)
@@ -61,9 +67,9 @@ get_unharmonised_metadata = function(metadata, ...){
     args = list(...)
     metadata |>
         collect() |>
-        group_by(file_id) |>
+        group_by(.data$file_id) |>
         summarise(
-            unharmonised = list(dataset_id=file_id[[1]], cells=cell_, conn=dbplyr::remote_con(metadata)) |>
+            unharmonised = list(dataset_id=.data$file_id[[1]], cells=.data$cell_, conn=dbplyr::remote_con(metadata)) |>
                 c(args) |> 
                 do.call(get_unharmonised_dataset, args=_) |> 
                 list()
