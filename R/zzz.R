6748d9f9029e791745d49070ac75a03421fcb1d6
Michael Milton <ttmigueltt@gmail.com>
2023-03-13 16:27:26 +1100

    Add missing unload hook

---
+++ R/zzz.R
@@ -0,0 +1,14 @@
+#' @importFrom purrr walk
+#' @importFrom dbplyr remote_con
+#' @importFrom DBI dbDisconnect
+.onUnload = function(libname, pkgname){
+    # Close connections to all cached tables. This should avoid most of the
+    # "Connection is garbage-collected" messages
+    cache$metadata_table |>
+        as.list() |>
+        purrr::walk(function(table){
+            table |>
+                remote_con() |>
+                dbDisconnect()
+        })
+}
