3a3d858ca26ee221c99e224d7cb9964571664441
Michael Milton <ttmigueltt@gmail.com>
2023-03-31 12:17:00 +1100

    Don't say we're downloading the metadata file each time

--- R/metadata.R
+++ R/metadata.R
@@ -134,13 +134,17 @@ get_metadata <- function(
         cached_connection
     }
     else {
-        report_file_sizes(remote_url)
         db_path <- file.path(cache_directory, "metadata.0.2.3.parquet")
-        sync_remote_file(
-            remote_url,
-            db_path,
-            progress(type = "down", con = stderr())
-        )
+        
+        if (!file.exists(db_path)){
+            report_file_sizes(remote_url)
+            sync_remote_file(
+                remote_url,
+                db_path,
+                progress(type = "down", con = stderr())
+            )
+        }
+
         table <- duckdb() |>
             dbConnect(drv = _, read_only = TRUE) |>
             tbl(db_path)
