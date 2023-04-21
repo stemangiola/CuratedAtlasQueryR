3355b697f82c3ee75786d2299f4e9bb54bc2c57c
Michael Milton <ttmigueltt@gmail.com>
2023-04-21 14:07:05 +1000

    Don't spell check vignettes

--- tests/spelling.R
+++ tests/spelling.R
@@ -1,3 +1,8 @@
 if(requireNamespace('spelling', quietly = TRUE))
-  spelling::spell_check_test(vignettes = TRUE, error = FALSE,
-                             skip_on_cran = TRUE)
+  spelling::spell_check_test(
+    # We hit https://github.com/ropensci/spelling/issues/62 if the 
+    # vignettes are checked
+    vignettes = FALSE,
+    error = FALSE,
+    skip_on_cran = TRUE
+ )
